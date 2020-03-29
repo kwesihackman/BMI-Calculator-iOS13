76fa3b2699a399af754cdff669945f064a9533ea
Eric Hackman <kwesihackman@gmail.com>
2020-03-29 09:52:01 +0000

    - Added the BMI struct - Updated the Calculator brain to have methods that return vmi value, color and advice - updated the result view controller to change the bmi value , background color and advice

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,18 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Eric Hackman on 29/03/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
+
---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Eric Hackman on 29/03/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CalculatorBrain {
+    var bmi:BMI?
+    
+    mutating func calculateBMI(with height:Float, and weight:Float) {
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5{
+            bmi = BMI(value: bmiValue, advice: "Underweight", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+        }else if bmiValue < 24.9{
+            bmi = BMI(value: bmiValue, advice: "Normal", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
+        }else{
+            bmi = BMI(value: bmiValue, advice: "Overweight", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        }
+    }
+    
+    func getBMIValue() ->  String{
+        return String(format: "%.1f", bmi?.value  ?? "0.0")
+        
+    }
+    
+    func getBMIAdvice() -> String {
+        return bmi?.advice ?? ""
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.clear
+    }
+}
